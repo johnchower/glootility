cc6120a2c4d1fb17c9fb8b02f0ca4eaaf2a0f79e
johnchower <johnchower@gmail.com>
2016-09-29 14:53:53 -0600

    Allow automatic detection of auth file

--- R/connect_to_database.r
+++ R/connect_to_database.r
@@ -2,44 +2,76 @@
 
 # Looker:
 
-connect_to_lookr <- function(auth_file_location = NULL, ...){
-  if(!is.null(auth_file_location)){
+#' Connect to Looker via the LookR package.
+#' 
+#' @param auth_file_location A character. Give the path of the directory where
+#' your authenticate file is located. If left blank, R will search for a 
+#' file named 'authenticate' in your current working directory. Set to NULL 
+#' if you want to enter your credentials manually.
+#' @return Connects your R session to the Looker API. No objects are returned.
+#' @examples
+#' connect_to_looker(auth_file_location="~/authfiles")
+
+connect_to_looker <- function(auth_file_location = 
+                               rprojroot::find_root(
+                                 rprojroot::has_file('authenticate')
+                               )
+                              , ...){
+  if(is.character(auth_file_location)){
     auth_info <- readLines(
                    paste(auth_file_location, "authenticate", sep="/")
                  )
     LookR::looker_setup(id = auth_info[1]
                         , secret = auth_info[2]
-                        , api_path = "https://api-looker.gloo.us/api/3.0") 
+                        , api_path = auth_info[3]) 
   } else {
     LookR::looker_setup(...
-                        , api_path = "https://api-looker.gloo.us/api/3.0") 
+                        , api_path = auth_info[3]) 
   }
 }
 
 # Postgres:
 
-connect_to_postgres <- function(auth_file_location = NULL, ...){
+#' Connect to Postgres via the RPostgreSQL package.
+#' 
+#' @param auth_file_location A character. Give the path of the directory where
+#' your authenticate file is located. If left blank, R will search for a 
+#' file named 'authenticate' in your current working directory. Set to NULL 
+#' if you want to enter your credentials manually.
+#' @return A list containing a driver object, drv, and a connection object,
+#' con.
+#' @examples
+#' connect_to_postgres(auth_file_location="~/authfiles")
+
+
+connect_to_postgres <- function(auth_file_location = 
+                                  rprojroot::find_root(
+                                    rprojroot::has_file('authenticate')
+                                  )
+                                , ...){
   driver <- DBI::dbDriver("PostgreSQL")
-  if(!is.null(auth_file_location)){
+  if(is.character(auth_file_location)){
     auth_info <- readLines(
                    paste(auth_file_location, "authenticate", sep="/")
                  )
     connection <- RPostgreSQL::dbConnect(
              driver
-             , dbname="polymer_production"
-             , host="localhost"
-             , port=5442
-             , user = auth_info[3]
-             , password = auth_info[4]
+             , dbname = auth_info[4]
+             , host = auth_info[5]
+             , port = auth_info[6]
+             , user = auth_info[7]
+             , password = auth_info[8]
            ) 
   } else {
     connection <- RPostgreSQL::dbConnect(
              driver
-             , dbname="polymer_production"
-             , host="localhost"
-             , port=5442
+             , dbname = auth_info[4]
+             , host = auth_info[5]
+             , port = auth_info[6]
              , ...
            ) 
   }
-  list(drv = driver, con = connection)
+  assign("postgres_connection"
+         , list(drv = driver, con = connection)
+         , envir = .GlobalEnv)
 }
