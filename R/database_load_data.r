fba0572c11b01f3019f7fb94783cc2909037012e
johnchower <johnchower@gmail.com>
2016-10-14 09:51:02 -0600

    Add run_look_list function

--- R/database_load_data.r
+++ R/database_load_data.r
@@ -7,6 +7,7 @@
 #'  See gloograph::test_queries for an example of how to format this list.
 #' @param connection The production database's PostgreSQL connection object.
 #' @return A named list of data frames, one for each (query_name, query) pair.
+#' @export
 
 run_query_list <- function(query_list
                            , connection = con){
@@ -31,3 +32,33 @@ run_query_list <- function(query_list
            , nm = namevec)
 }
 
+#' Run a list of looks and save the results in a list of
+#' data frames.
+#'
+#' @param look_list A list of (look_name, look_id) pairs.
+#'  See gloograph::test_looks for an example of how to format this list.
+#' @return A named list of data frames, one for each (look_name, look_id) pair.
+#' @export
+
+run_look_list <- function(look_list){
+
+  namevec <- 
+    unlist(
+      lapply(
+        look_list
+        , FUN = function(x){
+          x[['look_name']]
+      })
+    )
+
+  out <-
+    lapply(
+      look_list
+      , FUN = function(x){
+        LookR::run_look(x[['look_id']])
+    })
+
+  setNames(object = out 
+           , nm = namevec)
+
+}
