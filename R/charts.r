b9e3afff78a09b152d9d6bdcda3d2cba43c99e91
johnchower <johnchower@gmail.com>
2016-10-25 13:46:02 -0600

    Allow user to pass arguments to geom_bar

--- R/charts.r
+++ R/charts.r
@@ -13,6 +13,7 @@
 #' @param x_var, y_var Characters that name the columns of df to be used for
 #' the x and y variables, respectively.
 #' @param theme_tufte_opts Arguments to be passed to ggthemes::theme_tufte
+#' @param geom_bar_opts Arguments to be passed to ggplot2::geom_bar
 #' @return A ggplot object
 #' @export
 
@@ -20,10 +21,11 @@ tufte_bar_chart <- function(df
                             , ybreaks=0
                             , x_var, y_var
                             , theme_tufte_opts = list(base_size = 36, ticks = T)
+                            , geom_bar_opts = list(fill = "#888888", stat = "identity", width=.25)
                             , ...){
   ggplot2::ggplot(df, ggplot2::aes_string(x=x_var, y=y_var)) +
     do.call(ggthemes::theme_tufte, theme_tufte_opts) +
-    ggplot2::geom_bar(fill = "#888888", stat = "identity", width=.25) +
+    do.call(ggplot2::geom_bar, geom_bar_opts) +
     ggplot2::scale_y_continuous(breaks=ybreaks,...) +
     ggplot2::geom_hline(yintercept=ybreaks, col="white", lwd=1) +
     ggplot2::theme(axis.title = ggplot2::element_blank())
@@ -38,6 +40,7 @@ tufte_bar_chart <- function(df
 #' the x and y variables, respectively.
 #' @param group Character that determines which column of df to group by.
 #' @param theme_tufte_opts Arguments to be passed to ggthemes::theme_tufte
+#' @param geom_bar_opts Arguments to be passed to ggplot2::geom_bar
 #' @return A ggplot object
 #' @export
 
@@ -46,12 +49,11 @@ tufte_grouped_bar_chart <- function(df
                             , x_var, y_var 
                             , group
                             , theme_tufte_opts = list(base_size = 36, ticks = T)
+                            , geom_bar_opts = list(postition = "dodge", stat = "identity", width=.25)
                             , ...){
   ggplot2::ggplot(df, ggplot2::aes_string(x=x_var, y=y_var, fill = group)) +
     do.call(ggthemes::theme_tufte, theme_tufte_opts) +
-    ggplot2::geom_bar(position = "dodge"
-                      , stat = "identity"
-                      , width=.25) +
+    do.call(ggplot2::geom_bar, geom_bar_opts) +
     ggplot2::scale_fill_manual(
       values=c(glootility::gloo_colors[['gray']]
                , glootility::gloo_colors[['blue']])
