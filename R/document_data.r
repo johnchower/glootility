48d37c5f20d3d1343420eac8c85e78abf8ecc1ba
johnchower <johnchower@gmail.com>
2016-11-20 14:41:40 -0700

    Add dataset containing all legitimate gloo internal user emails. (No test accounts.)

---
+++ R/document_data.r
@@ -0,0 +1,8 @@
+#' Gloo internal emails
+#'
+#' A caracter vector containing all emails associated to Gloo employees'
+#' 'growing selves'.
+#'
+#' @format A character vector.
+#' @export
+"gloo_user_emails"
