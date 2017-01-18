9f36708e61b5c7725acae891c96251d36be2bca5
johnchower <johnchower@gmail.com>
2017-01-18 12:11:05 -0700

    Document pa_flash_cat query

--- R/document_data.r
+++ R/document_data.r
@@ -4,5 +4,12 @@
 #' 'growing selves'.
 #'
 #' @format A character vector.
-
 "gloo_user_emails"
+
+#' Platform action flash report category query.
+#'
+#' Creates a temporary table in Redshift that matches platform actions to their
+#' Flash Report categories.
+#'
+#' @format A character vector.
+"query_pa_flash_cat"
